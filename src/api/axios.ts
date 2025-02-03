df13e31df69b8d6a7d9aff92525e69537bf84ff0
Jer403 <jeremiwtfxd@gmail.com>
2025-02-02 20:45:13 -0500

    Updated apiurl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://3dcute.up.railway.app//app";
+const API_URL = "https://3dcute.up.railway.app/app";
 
 const instance = axios.create({
   baseURL: API_URL,
